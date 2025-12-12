c92400e02997875fa36c8663f483e597be70ff32
jsmart564 <Adeyemijuwon0@gmail.com>
2025-12-12 01:49:22 +0000

    Add files via upload

---
+++ juwonexercise1-3.py
@@ -0,0 +1,32 @@
+#exercise 1. A good firsr program
+print("Hello World!")
+print("Hello Again")
+print("i like typing this.")
+print("this is fun.")
+print('yay! printing.')
+print("i'd much rather you 'not'.")
+print('I "said" do not this.')
+
+#exercise 2. Comments and Pound Characters
+# a comment.  This is so you can read your program later.
+# Anything after the # is ignored by python.
+print("i could have code like this") # and the comment after is ignored
+# You can also use a comment to "disable" or comment out
+# print*(*"this will not run"*)*
+print("this will run.")
+
+#exercise 3. Numbers and Math
+print("i will not count my chickens:")
+print("hens", 25 + 30 / 6)
+print("roosters", 100 - 25 * 3 % 4)
+print("now i will count the eggs:")
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6) 
+print("is it true that 3 + 2 < 5 - 7?")
+print(3 + 2 < 5 - 7)
+print("what is 3 + 2?", 3 + 2)
+print("what is 5 - 7?", 5 - 7)
+print("oh, that's why it's False.")
+print("how about some more.")
+print("is it greater?", 5 > -2)
+print("is it greater or equal?", 5 >= -2)
+print("is it less or equal?", 5 <= -2)
